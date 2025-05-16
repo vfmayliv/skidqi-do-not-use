cceb71590e17b163c2d2a0a503a6c681d18392ff
vfmayliv <videofootball.ru@gmail.com>
2025-05-17 02:19:21 +0500

    Обновление конфигурации Vite для поддержки GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "localhost",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  base: '/skidqi-kz/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
-  }
+  },
 });
